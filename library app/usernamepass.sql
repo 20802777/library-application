38caf686edc539a4116eed291e35f42116c4dd03
SeyedehMahissa Salehnezhad <148220693+20802777@users.noreply.github.com>
2024-04-05 09:38:19 +0300

    Delete library app directory

--- library app/usernamepass.sql
+++
@@ -1,7 +0,0 @@
-create table loginTable(
-    id int NOT NULL  primary key,
-    username varchar(150) not null,
-    pass varchar(150) not null
-)
-
-insert into loginTable (id,username,pass) values (1,'mahisa','m2001');
